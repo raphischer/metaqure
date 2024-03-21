3c9afeb2c6ecc2d8b5bb1ddc6e63d794b76e6415
Raphael Fischer <27725573+raphischer@users.noreply.github.com>
2024-03-21 19:16:52 +0100

    fixed bl res 2

--- process_logs.py
+++ process_logs.py
@@ -53,7 +53,9 @@ if __name__ == "__main__":
     database.to_pickle(db_file)
 
     dbs = [pd.read_pickle(os.path.join(args.db_dir, fname)) for fname in os.listdir(args.db_dir) if '.pkl' in fname and fname not in ['complete.pkl', 'baselines.pkl', 'subset.pkl']]
-    complete = pd.concat(dbs).dropna().reset_index()
+    complete = pd.concat(dbs)
+    complete['parameters'] = complete['parameters'].fillna(0) # nan parameters are okay (occurs for PFN)
+    complete = complete.dropna() # get rid of failed PFN evals
     # for some weird outlier cases (< 4%), codecarbon logged extreeemely high and unreasonable consumed energy (in the thousands and even millions of Watt)
     # we discard these outliers (assuming a max draw of 400 Watt) and do a simple kNN gap filling
     complete.loc[complete['train_power_draw'] / complete['train_running_time'] > 400,'train_power_draw'] = np.nan
